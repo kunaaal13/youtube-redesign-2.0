7cb9b9d32dd1156a63511a7ccc5a85dc0e1eccdd
Kunaaal13 <mrkunalyadav7@gmail.com>
2022-10-22 13:54:17 +0530

    search fixes

--- components/Results/VideoCard.js
+++ components/Results/VideoCard.js
@@ -17,7 +17,7 @@ function VideoCard({
   const [imageError, setImageError] = useState(false)
   dayjs.extend(relativeTime)
   let text = description.trim()
-  text = text.replace(/\s\s+/g, '')
+  text = text.replace(/  |\r\n|\n|\r/gm, '')
 
   return (
     <Link
@@ -27,34 +27,36 @@ function VideoCard({
       }}
     >
       <div
-        className={`mb-5 flex w-full items-center justify-start sm:pr-5 md:pr-20 ${
+        className={`mb-5 flex w-full max-w-full flex-col items-center justify-start sm:flex-row lg:pr-20 ${
           imageError ? 'hidden' : ''
         } ${i === total - 1 ? 'mb-20' : ''}`}
       >
-        <div className='relative h-36 w-[240px] cursor-pointer rounded-md border sm:w-80 md:h-48 md:w-[22rem]'>
+        <div className='relative h-52 w-full cursor-pointer rounded-md border sm:h-48 sm:w-[27rem] sm:min-w-[350px] sm:max-w-[350px]'>
           <Image
             src={url}
             onError={() => setImageError(true)}
             alt={`Thumbnail for`}
             layout='fill'
-            className={`h-full w-full rounded-md object-cover`}
+            className={`relative w-full rounded-md object-cover`}
           />
         </div>
 
-        <div className='ml-2 flex h-36 flex-1 flex-col items-start md:ml-7 md:h-48'>
-          <h3 className='text-start font-semibold line-clamp-2 md:text-lg'>
+        <div className='mt-3 flex flex-col items-start sm:ml-7 sm:h-48'>
+          <h3 className='text-start font-semibold line-clamp-2 sm:mb-2 md:text-lg'>
             {title}
           </h3>
 
-          <h3 className='my-[0.3rem] text-sm text-[#606060] md:text-base'>
-            {channel}
-          </h3>
+          <div className='flex flex-row space-x-2 space-y-0 sm:flex-col sm:items-start sm:space-y-1 sm:space-x-0'>
+            <h3 className='text-sm text-[#606060] line-clamp-1 md:text-base'>
+              {channel}
+            </h3>
 
-          <p className='my-[0.3rem] text-sm text-[#606060]'>
-            {dayjs().to(dayjs(timestamp))}
-          </p>
+            <p className='text-sm text-[#606060]'>
+              {dayjs().to(dayjs(timestamp))}
+            </p>
+          </div>
 
-          <p className='text-start text-sm text-[#606060] line-clamp-2'>
+          <p className='mt-1 text-start text-sm text-[#606060] line-clamp-2 sm:mt-3'>
             {text}
           </p>
         </div>
