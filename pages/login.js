ca62ae345c8fcacb977910d5a5e1809e9f4f44b4
kunaaal13 <84015751+kunaaal13@users.noreply.github.com>
2022-11-15 19:26:34 +0530

    login page text color change

--- pages/login.js
+++ pages/login.js
@@ -1,45 +1,45 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import React, { useEffect } from 'react'
-import { useSession, signIn, signOut } from 'next-auth/react'
-import { useRouter } from 'next/router'
+import Head from 'next/head';
+import Image from 'next/image';
+import React, { useEffect } from 'react';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 function Login() {
-  const { data: session } = useSession()
-  const router = useRouter()
+  const { data: session } = useSession();
+  const router = useRouter();
 
   useEffect(() => {
     if (session) {
-      router.push('/')
+      router.push('/');
     }
-  })
+  });
 
   return (
-    <div className='flex h-screen w-full flex-col items-center justify-center bg-gradient-to-tl from-gray-700 via-gray-900 to-black'>
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-gradient-to-tl from-gray-700 via-gray-900 to-black">
       <Head>
         <title>Login || Youtube redesign</title>
-        <meta name='description' content='Youtube redesign' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="Youtube redesign" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Image src='/logo.svg' alt='' height={150} width={150} />
+      <Image src="/logo.svg" alt="" height={150} width={150} />
 
       <div
         onClick={() => {
-          signIn()
+          signIn();
         }}
-        className='hover:text-whit mb-20 cursor-pointer rounded-md border border-neutral-500 px-10 py-5 shadow-md hover:bg-cyan-900/20'
+        className="hover:text-whit mb-20 cursor-pointer rounded-md border border-neutral-500 px-10 py-5 shadow-md hover:bg-cyan-900/20"
       >
-        <h1 className='text-2xl font-bold text-white'>Sign in with Google</h1>
+        <h1 className="text-2xl font-bold text-white">Sign in with Google</h1>
       </div>
 
-      <footer className='absolute bottom-10'>
-        <div className=''>
-          <h1 className='text-center text-2xl'>Made with ❤️ by Kunaaal</h1>
+      <footer className="absolute bottom-10">
+        <div className="">
+          <h1 className="text-center text-2xl text-white">Made with ❤️ by Kunaaal</h1>
         </div>
       </footer>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
